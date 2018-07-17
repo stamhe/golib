943afdf229271518dc1a222bf0044885d39f9009
Stam He <stamhe@163.com>
2018-07-17 15:52:40 +0800

    renew

---
+++ pack1.go
@@ -0,0 +1,8 @@
+package golib
+
+var Pack1Int int = 42
+var Pack1Float = 3.14
+
+func ReturnStr() string {
+    return "Hello Main from package pack1"
+}
---
+++ pack2.go
@@ -0,0 +1,8 @@
+package golib
+
+var Pack2Int int = 84
+var Pack2Float = 6.28
+
+func ReturnStr2() string {
+    return "Hello Main from package pack2"
+}
